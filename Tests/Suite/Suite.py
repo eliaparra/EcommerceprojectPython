4648863ac33c590d1d42f516ec1905b22f58e3d9
Elia <theqacorner1@gmail.com>
2020-10-23 16:40:00 +0200

    little changes

--- Tests/Suite/Suite.py
+++ Tests/Suite/Suite.py
@@ -1,6 +1,9 @@
+
+
+import HTMLTestRunner
 import unittest
 
-
+import fp as fp
 
 from Tests.Test_1 import Test_1
 from Tests.Test_10 import Test_10
@@ -18,9 +21,26 @@ from Tests.Test_7 import Test_7
 from Tests.Test_8 import Test_8
 from Tests.Test_9 import Test_9
 
-suite = unittest.TestSuite()
-suite.addTests(
-    [Test_1(), Test_2(), Test_3(), Test_4(), Test_5(), Test_6(), Test_7(), Test_8(), Test_9(), Test_10(), Test_11(),
-     Test_12(), Test_13(), Test_14(), Test_15()])
 
-unittest.TextTestRunner().run(suite)
+suite1 = unittest.TestLoader().loadTestsFromTestCase(Test_1)
+suite2 = unittest.TestLoader().loadTestsFromTestCase(Test_2)
+suite3 = unittest.TestLoader().loadTestsFromTestCase(Test_3)
+suite4 = unittest.TestLoader().loadTestsFromTestCase(Test_4)
+suite5 = unittest.TestLoader().loadTestsFromTestCase(Test_5)
+suite6 = unittest.TestLoader().loadTestsFromTestCase(Test_6)
+suite7 = unittest.TestLoader().loadTestsFromTestCase(Test_7)
+suite8 = unittest.TestLoader().loadTestsFromTestCase(Test_8)
+suite9 = unittest.TestLoader().loadTestsFromTestCase(Test_9)
+suite10 = unittest.TestLoader().loadTestsFromTestCase(Test_10)
+suite11 = unittest.TestLoader().loadTestsFromTestCase(Test_11)
+suite12 = unittest.TestLoader().loadTestsFromTestCase(Test_12)
+suite13 = unittest.TestLoader().loadTestsFromTestCase(Test_13)
+suite14 = unittest.TestLoader().loadTestsFromTestCase(Test_14)
+suite15 = unittest.TestLoader().loadTestsFromTestCase(Test_15)
+
+
+all_tests = unittest.TestSuite([suite1, suite2,suite3, suite4, suite5, suite6, suite7, suite8, suite9, suite10, suite11, suite12, suite13, suite14, suite15])
+
+html_report_dir = './html_report'
+unittest.main(testRunner=HTMLTestRunner.HTMLTestRunner(output=html_report_dir,combine_reports=True))
+
